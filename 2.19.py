431646a745b5780fdadff9e0d241f28a1b05d51d
xntentation <82881375+xntentation@users.noreply.github.com>
2022-10-26 11:05:16 +0200

    Update 2.19.py

--- 2.19.py
+++ 2.19.py
@@ -1,5 +1,2 @@
 L = [1, 21, 321]
-print(str(L[0]).zfill(3) + ' ' +  str(L[1]).zfill(3)), print(L[2])
-
-
-#print(str(L[0]).zfill(3) + ' ' +  str(L[1]).zfill(3)) + str(L[3])) czemu nie mogę domknąć nawiasu?
+print(str(L[0]).zfill(3) + ' ' +  str(L[1]).zfill(3) + ' '+ str(L[2]))
